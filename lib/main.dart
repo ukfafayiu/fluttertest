047b0c0c8e02ce3106913c9be02c4f3e09d2e44b
Arthur Yiu <ukfafayiu@gmail.com>
2022-11-27 11:51:57 +0000

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+const int httpRequestTimeoutPeriod = 1; //Set Http Request Timeout (Second)
+const String sUrlAuthority = 'www.reddit.com';
+const String sUrlPath = '/r/FlutterDev.json';
 
 void main() {
   runApp(const MyApp());
@@ -49,6 +56,49 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  dynamic _jsonResponse;
+  String sTitle = '', sSubTitle = '';
+
+  void _readJsonFromReddit() async {
+    var url = Uri.https(sUrlAuthority, sUrlPath);
+    dynamic jsonResponse;
+    _counter = 0;
+
+    setState(() {
+    });
+
+    var response = await http.get(url).timeout(
+      const Duration(seconds: httpRequestTimeoutPeriod),
+      onTimeout: () {
+        return http.Response(
+            'Request Timeout ($httpRequestTimeoutPeriod seconds)', 408); // Request Timeout response status code
+      },
+    );
+    if (response.statusCode == 200) {
+      jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      _counter = jsonResponse['data']['dist'];
+      _jsonResponse = jsonResponse;
+      debugPrint(
+          'Number of post read from /r/FlutterDev: ${_counter.toString()}.');
+    } else {
+      debugPrint('Request failed with status: ${response.statusCode} - ${response.body}.');
+      alertMessage(response.body, response.statusCode.toString());
+    }
+
+    setState(() {});
+  }
+
+  Future<void> alertMessage( String parTitle, String parDesc ) async {
+    await Alert(context: context,
+        title: parTitle,
+        desc: parDesc,
+        buttons: [
+          DialogButton(onPressed: () => Navigator.pop(context), width: 100,child: const Text(
+            'OK', style: TextStyle(color: Colors.white, fontSize: 10),),
+          )
+        ]
+    ).show();
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -78,38 +128,32 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Scrollbar(
+          child: ListView.builder(
+              itemCount: _counter,
+              itemBuilder: (context, i) {
+                sTitle = _jsonResponse['data']['children'][i]['data']['title'];
+                sSubTitle = _jsonResponse['data']['children'][i]['data']['selftext'];
+                return Column(
+                  children: [
+                    ListTile(
+                        title: Text(sTitle),
+                        subtitle: Text(sSubTitle)
+                    ),
+                    const Divider()
+                  ],
+                );
+              }),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _readJsonFromReddit,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
+
 }
